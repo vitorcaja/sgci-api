1311eeb1faa0e7d6b89240f9c0492ca7a41667e3
Vitor Cajá <vitor.caja@gmail.com>
2024-12-31 00:54:15 -0300

    feat: inicio projeto SGCI.

---
+++ src/main/java/br/com/vitorcaja/sgci/controller/ImovelController.java
@@ -0,0 +1,16 @@
+package br.com.vitorcaja.sgci.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("imoveis")
+public class ImovelController {
+
+    @GetMapping
+    public ResponseEntity<String> findAll(){
+        return ResponseEntity.ok("Ok!");
+    }
+}
